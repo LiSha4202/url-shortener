14ef06d1e69f5c95a0d7d1d896f68fd0622be066
liSha4202 <dr.aleksandr-sirotin@yandex.ru>
2026-07-08 19:25:41 +0300

    Create Declarative Base

--- src/models/base.py
+++ src/models/base.py
@@ -1,5 +1,18 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy import MetaData
+
+from core.config import settings
+
+from utils.case_converter import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
