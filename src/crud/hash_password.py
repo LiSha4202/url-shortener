28741f8c6aac983c5bb8f181e597d0fa958f5e0c
liSha4202 <dr.aleksandr-sirotin@yandex.ru>
2026-07-17 16:51:44 +0300

    Use Argon2 Scheme for hashing password

--- src/crud/hash_password.py
+++ src/crud/hash_password.py
@@ -1,11 +1,14 @@
 from passlib.context import CryptContext
+from hashlib import sha256
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def get_password_hash(password: str) -> str:
+    """Хэширует пароль через Argon2"""
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Проверяет пароль"""
+    return pwd_context.verify(plain_password, hashed_password)
