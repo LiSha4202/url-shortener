e59b5eed1c696bf909fb063548099cba417f672c
liSha4202 <dr.aleksandr-sirotin@yandex.ru>
2026-07-17 15:58:29 +0300

    Add passlib and update create_user(password now hash)

--- src/crud/user_crud.py
+++ src/crud/user_crud.py
@@ -4,11 +4,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models.users_model import User  # SQLAlchemy модель
 from core.schemas.user_schema import UserCreate  # Pydantic схема
 
+from .hash_password import get_password_hash
+
 
 async def create_user(session: AsyncSession, user: UserCreate):
     """Создание нового пользователя"""
 
-    new_user = User(**user.model_dump())
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
+    )
     session.add(new_user)
     await session.commit()
     return new_user
