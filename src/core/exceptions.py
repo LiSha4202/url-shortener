7ac4ad201b06e1af78b10230af44d382b8c7cbca
liSha4202 <dr.aleksandr-sirotin@yandex.ru>
2026-07-25 19:31:34 +0300

    Add ID for exceptions

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 def exc_401_not_val_cred(header_type: str):
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail="Could not validate credentials [ID-1]",
         headers={"WWW-Authentificate": header_type},
     )
 
@@ -12,26 +12,26 @@ def exc_401_not_val_cred(header_type: str):
 def exc_link_404_not_found():
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Link not found",
+        detail="Link not found [ID-2]",
     )
 
 
 def exc_link_410_gone():
     raise HTTPException(
         status_code=status.HTTP_410_GONE,
-        detail="Link expired",
+        detail="Link expired [ID-3]",
     )
 
 
 def exc_log_click_500_server_error():
     raise HTTPException(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail="Failed to log click",
+        detail="Failed to log click [ID-4]",
     )
 
 
 def exc_short_code_existing(shortcode: str):
     raise HTTPException(
         status_code=status.HTTP_409_CONFLICT,
-        detail=f"Short code '{shortcode}' is already in use",
+        detail=f"Short code '{shortcode}' is already in use [ID-5]",
     )
