97bf4720ad0d927fab03357a8e1e161dda4096b9
liSha4202 <dr.aleksandr-sirotin@yandex.ru>
2026-08-08 17:34:03 +0300

    Function increment_click_count now worked

--- src/crud/link_crud.py
+++ src/crud/link_crud.py
@@ -97,26 +97,25 @@ async def get_all_links(session: AsyncSession) -> list[Link]:
 async def increment_click_count(session: AsyncSession, short_code: str) -> bool:
     """Увеличение количества кликов по ссылке"""
 
-    short_code_db_link = await get_link_by_code(session, short_code)
-    if not short_code_db_link:
+    now = datetime.now(timezone.utc)
+    today = date.today().isoformat()
+
+    # 1. Получаем ссылку
+    stmt = select(Link).where(Link.short_code == short_code)
+    result = await session.execute(stmt)
+    link = result.scalar_one_or_none()
+    if not link:
         return False
 
-    # Увеличиваем счётчик перехода по ссылке
-    short_code_db_link.clicks_count += 1
-    now = datetime.now(timezone.utc)  # Записываем текущее время
+    # 2. Обновляем статистику
+    link.clicks_count += 1  # Общее количество кликов
+    if link.first_click is None:  # Если не было 1 клика
+        link.first_click = now  # ... то это первый
+    link.last_click = now  # сохраняем последний
 
-    if not short_code_db_link.first_click:  # Если это первое нажатие на ссылку...
-        short_code_db_link.first_click = now  # ...то записываем время первого клика
-    short_code_db_link.last_click = now  # Обновляем последнее время клика
-
-    # Теперь записываем в статистику clicks_by_day
-    today = date.today().isoformat()  # получаем сегодняшнюю дату
-    day_data = (
-        short_code_db_link.clicks_by_day or {}
-    )  # Получение данных за день или создание нового словаря
-    day_data[today] = (
-        day_data.get(today, 0) + 1
-    )  # Увеличиваем количество переходов сегодняшенй даты на 1
+    day_data = link.clicks_by_day or {}  # Получаем данные по дням
+    day_data[today] = day_data.get(today, 0) + 1  # Увеличиваем количество
+    link.clicks_by_day = day_data  # Сохранение
 
     await session.commit()
     return True
